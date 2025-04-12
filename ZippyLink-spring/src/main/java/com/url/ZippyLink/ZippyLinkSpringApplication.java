5a73450f165d4f40bb5cc5803aa22150acb81945
aansh06 <aansh0611@gmail.com>
2025-04-13 00:21:09 +0530

    spring setup

---
+++ ZippyLink-spring/src/main/java/com/url/ZippyLink/ZippyLinkSpringApplication.java
@@ -0,0 +1,13 @@
+package com.url.ZippyLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZippyLinkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZippyLinkSpringApplication.class, args);
+	}
+
+}
---
+++ ZippyLink-spring/src/test/java/com/url/ZippyLink/ZippyLinkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.ZippyLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZippyLinkSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
